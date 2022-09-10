5a12e4bc031027d8c9b530790728d9e9f6cb16f5
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-09-10 12:37:19 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const body = document.querySelector("body");
+function submitData(username,usermail) {
+    const data = {
+        name: username,
+        email: usermail
+    };
+
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+
+    let allData = fetch("http://localhost:3000/users", configObject)
+        .then(res => res.json())
+        .then(json => {
+            body.append(json.id);
+        })
+        .catch(error => {
+            alert("A bug detected!");
+            body.append(error)
+        });
+
+    return allData;
+}
